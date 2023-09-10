0cfb67ef8899ecf526315412737253219edb9b66
MuhammadMoiz2050 <moiz2050@gmail.com>
2023-09-10 17:52:56 +0500

    Add loading time on landing page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,31 +7,35 @@ import { useNavigate } from 'react-router-dom';
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate()
-const handleButton=(e)=>{
-  navigate('/cli');
-}
+  const handleButton = (e) => {
+    setIsLoading(true);
+    setTimeout(() => { navigate('/cli'); }, 1000)
+
+
+  }
 
   return (
     <div className='animated-background'>
-    <div className="container">
-      <div className="columns is-centered is-vcentered" style={{ height: '100vh' }}>
-        <div className="column is-half">
-          <div className="card"   >
-            <div className="card-content has-text-centered">
-              <p className=" has-background-white title">Welcome!</p>
+      <div className="container">
+        <div className="columns is-centered is-vcentered" style={{ height: '100vh' }}>
+          <div className="column is-half">
+            <div className="card"   >
+              <div className="card-content has-text-centered">
+                <p className=" has-background-white title">Welcome!</p>
+              </div>
             </div>
-          </div>
-          <div className="card-footer">
-          <button
-                className="card-footer-item button is-primary is-info"
+            <div className="card-footer">
+              <button
+                className={`card-footer-item button is-primary is-info ${isLoading ? 'is-loading' : 'is-primary'}`}
                 onClick={handleButton}
               > Start
               </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
